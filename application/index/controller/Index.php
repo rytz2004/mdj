cc5e55ccc7d8ceeabd66a9df0ce22be93922b381
renyuan <renyuan@inke.cn>
2019-01-23 10:13:53 +0800

    code change

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,11 +1,12 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
-
+use think\db;
 class Index extends Controller
 {
     public function index()
     {
+      //db::find()
       return $this->fetch("rege");
      }
 
