9007906306c642cce309acab918d5e89f284b80b
renyuan <renyuan@inke.cn>
2019-01-23 16:51:24 +0800

    change code

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,12 +1,11 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
-use think\db;
+use think\Db;
 class Index extends Controller
 {
     public function _initialize()
     {
-        // 判断用户是否登陆
         $user = isset($_SESSION['name']) ? $_SESSION['name'] :null;
         if(!$user){
            // $this->success('未登录，请登录',url('index/index'),3);
@@ -15,7 +14,8 @@ class Index extends Controller
 
     public function index()//http://mdj.cn/index/Index/index
    {
-      return $this->fetch("index");
+       // 判断用户是否登陆
+       return $this->fetch("index");
     }
 
     public function login(){//http://mdj.cn/index/Index/login
