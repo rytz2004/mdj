527ff503c13d38de2fe45900cccfb507d68a448a
renyuan <renyuan@inke.cn>
2019-01-23 11:52:41 +0800

    修改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,11 +4,19 @@ use think\Controller;
 use think\db;
 class Index extends Controller
 {
-    public function index()
+    public function _initialize()
     {
-      //db::find()
-      return $this->fetch("rege");
-     }
+        // 判断用户是否登陆
+        $user = isset($_SESSION['name']) ? $_SESSION['name'] :null;
+        if(!$user){
+           // $this->success('未登录，请登录',url('index/index'),3);
+        }
+    }
+
+    public function index()//http://mdj.cn/index/Index/index
+   {
+      return $this->fetch("index");
+    }
 
     public function login(){//http://mdj.cn/index/Index/login
         return $this->fetch("login");
